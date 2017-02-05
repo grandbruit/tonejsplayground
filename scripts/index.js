06fd8085893838bc586b79d16f9ee7b6ed0224b6
Étienne Després <etienne@pochour.com>
2017-02-04 23:52:08 -0500

    Persist field values to local storage

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,30 @@ var instrument = document.getElementById('instrument');
 var note = document.getElementById('note');
 var play = document.getElementById('play');
 
-// Event listeners
+// Restore fields from local storage
+if (localStorage.getItem('instrument')) {
+  instrument.value = localStorage.getItem('instrument');
+}
+
+if (localStorage.getItem('note')) {
+  note.value = localStorage.getItem('note');
+}
+
+// Save field values to local storage when they change
+instrument.addEventListener('change', function() {
+  localStorage.setItem('instrument', this.value);
+});
+
+note.addEventListener('input', function() {
+  localStorage.setItem('note', this.value);
+});
+
+// Handle click for Play button
 play.addEventListener('mousedown', function() {
   window[instrument.value].triggerAttack(note.value);
-})
+});
 
 play.addEventListener('mouseup', function() {
   window[instrument.value].triggerRelease();
-})
+});
+
