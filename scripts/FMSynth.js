b1681c2f2cdea804d75304b4167f136a9269de4e
Étienne Després <etienne@pochour.com>
2017-02-04 23:54:50 -0500

    Add FMSynth

---
+++ scripts/FMSynth.js
@@ -0,0 +1,23 @@
+FMSynth = new Tone.FMSynth({
+  "harmonicity": 3,
+  "modulationIndex": 10,
+  "detune": 0,
+  "oscillator": {
+    "type": "sine"
+  },
+  "envelope": {
+    "attack": 0.01,
+    "decay": 0.01,
+    "sustain": 1,
+    "release": 0.5
+  },
+  "modulation": {
+    "type": "square"
+  },
+  "modulationEnvelope": {
+    "attack": 0.5,
+    "decay": 0.0,
+    "sustain": 1,
+    "release": 0.5
+  }
+}).toMaster();
