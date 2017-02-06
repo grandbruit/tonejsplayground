1832b1d0e3b271a86f3a9c8f273ce65ed3092e45
Étienne Després <etienne@pochour.com>
2017-02-05 19:39:40 -0500

    Add Synth

---
+++ scripts/Synth.js
@@ -0,0 +1,11 @@
+Synth = new Tone.Synth({
+	"oscillator" : {
+		"type" : "triangle"
+	},
+	"envelope" : {
+		"attack" : 0.005,
+		"decay" : 0.1,
+		"sustain" : 0.3,
+		"release" : 1
+	}
+}).toMaster();
